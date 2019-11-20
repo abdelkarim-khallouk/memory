e94ede120dc29af39e19b57b0faf19149dbdc8a4
abdelkarim-khallouk <khlgeek@gmail.com>
2019-11-20 15:01:20 +0100

    - Describe an element with JSX - click event

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import './App.css'
 import GuessCount from './GuessCount'
 import Card from './Card'
 
+
+
+
 class App extends Component {
+
+  handleCardClick(card){
+    console.log(card, "Clicked");
+  }
+
   render() {
+    
+    const won = new Date().getSeconds() % 2 === 0
+
     return (
       <div className="memory" >
         <GuessCount guesses={3}></GuessCount>
-        <Card card="💣" feedback="hidden" />
-        <Card card="❤️" feedback="justMatched" />
-        <Card card="♠️" feedback="justMismatched" />
-        <Card card="😀" feedback="visible" />
-        <Card card="🔔" feedback="justMatched" />
-        <Card card="💰" feedback="hidden" />
+        <Card card="💣" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="❤️" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card card="♠️" feedback="justMismatched" onClick={this.handleCardClick} />
+        <Card card="😀" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="💰" feedback="hidden" onClick={this.handleCardClick} />
+        { won ? <p>Bravo, Gagné !</p> : <p>Dommage, Perdu !!</p> }
       </div>
+      
     )
   }
 }
